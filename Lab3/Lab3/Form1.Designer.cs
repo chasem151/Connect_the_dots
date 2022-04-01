8280863418d2a6c221e9c5fe72b1a9c0cd6f7c36
unknown <maivaldc2@gmail.com>
2022-04-01 12:55:23 -0400

    Everything working

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Linebutton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.upToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Linebutton
+            // 
+            this.Linebutton.Location = new System.Drawing.Point(44, 182);
+            this.Linebutton.Name = "Linebutton";
+            this.Linebutton.Size = new System.Drawing.Size(99, 23);
+            this.Linebutton.TabIndex = 0;
+            this.Linebutton.Text = "Show Lines";
+            this.Linebutton.UseVisualStyleBackColor = true;
+            this.Linebutton.Click += new System.EventHandler(this.Linebutton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2000, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.upToolStripMenuItem,
+            this.exToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // upToolStripMenuItem
+            // 
+            this.upToolStripMenuItem.Name = "upToolStripMenuItem";
+            this.upToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.upToolStripMenuItem.Text = "up";
+            // 
+            // exToolStripMenuItem
+            // 
+            this.exToolStripMenuItem.Name = "exToolStripMenuItem";
+            this.exToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exToolStripMenuItem.Text = "ex";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(2000, 1000);
+            this.ClientSize = new System.Drawing.Size(1924, 974);
+            this.Controls.Add(this.Linebutton);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Linebutton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem upToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exToolStripMenuItem;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        private List<PointF> coordinates = new List<PointF>();
+        bool flag = false;
+        
+        public Form1()
+        {
+            this.DoubleBuffered = true;
+            
+            //this.AutoScroll = true;
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+                PointF p = new PointF(e.X, e.Y);
+                
+                this.coordinates.Add(p);
+                this.Invalidate();
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                this.coordinates.Clear();
+                this.Invalidate();
+            }
+        }
+
+        
+
+        private void Linebutton_Click(object sender, EventArgs e)
+        {
+
+            if (Linebutton.Text == "Show Lines")
+            {
+                Linebutton.Text = "Hide Lines";
+
+                //this.Invalidate();
+            }
+            else if (Linebutton.Text == "Hide Lines")
+            {
+                Linebutton.Text = "Show Lines";
+
+                //this.Invalidate();
+            }
+
+            if (flag)
+            {
+                flag = false;
+                //this.Invalidate();
+            }
+            else
+            {
+                flag = true;
+                //this.Invalidate(); 
+            }
+            Invalidate();  
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Pen blk = new Pen(Brushes.Black);
+            blk.Width = 2.0F;
+            //blk.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+            if (flag)
+            {
+                for(int i=0; i < this.coordinates.Count; i++)
+                {
+                    if (i != 0)
+                        --i;
+                    e.Graphics.DrawLine(blk, this.coordinates[i], this.coordinates[++i]);
+                    Invalidate();
+                    //e.Graphics.DrawString("heck", Font, Brushes.Black, this.coordinates[i].X, this.coordinates[i].Y);
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            const int WIDTH = 15;
+            const int HEIGHT = 15;
+            Graphics g = e.Graphics;
+           
+            foreach (PointF p in this.coordinates)
+            {
+                g.FillEllipse(Brushes.Red, p.X - WIDTH / 2, p.Y - WIDTH / 2, WIDTH, HEIGHT);
+                Invalidate();
+            }
+           //this.Invalidate();
+
+            //base.OnPaint(e);
+            
+            //Point p2 = new Point(100, 100);
+            //int ct = coordinates.Count;
+            /*
+            
+            */
+
+        }
+
+        protected override Point ScrollToControl(Control activeControl)
+        {
+            Point pt = this.AutoScrollPosition;
+            return pt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
